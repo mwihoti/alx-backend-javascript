861fd05f3521827033abd9dad093074b2276b062
mwihoti <danielmwihoti@gmail.com>
2024-09-19 12:00:13 +0300

    HTTP server using Express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello Holberton school');
+});
+
+app.listen(1245, () => {
+  console.log('Server runnig');
+});
