aaf22e279e1c2b223d3f8bd82075429d35e2c3d9
mwihoti <danielmwihoti@gmail.com>
2024-09-19 14:08:50 +0300

    a more complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,28 +1,81 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello Holberton school!');
+const PORT = 1245;
+const DB_FILE = process.argv[2] || '';
+
+// Function to count students from the CSV file
+const countStudents = (dataPath) => new Promise((resolve, reject) => {
+  if (!dataPath) {
+    reject(new Error('Cannot load the database'));
+    return;
+  }
+
+  fs.readFile(dataPath, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const lines = data.trim().split('\n');
+    if (lines.length <= 1) {
+      resolve('Number of students: 0');
+      return;
+    }
+
+    const studentGroups = {};
+    const headers = lines[0].split(',');
+    const firstNameIndex = headers.indexOf('firstname');
+    const fieldIndex = headers.indexOf('field');
+
+    lines.slice(1).forEach((line) => {
+      const columns = line.split(',');
+      if (columns.length > 1) {
+        const firstName = columns[firstNameIndex];
+        const field = columns[fieldIndex];
+        if (!studentGroups[field]) {
+          studentGroups[field] = [];
+        }
+        studentGroups[field].push(firstName);
+      }
+    });
+
+    const totalStudents = Object.values(studentGroups).reduce((acc, curr) => acc + curr.length, 0);
+    const reportParts = [`Number of students: ${totalStudents}`];
+    Object.entries(studentGroups).forEach(([field, names]) => {
+      reportParts.push(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+    });
+
+    resolve(reportParts.join('\n'));
+  });
 });
 
-app.get('/students', (req, res) => {
-    const dbFilePath = process.argv[2];
-    res.write('This is the list of our students\n');
-    countStudents(dbFilePath)
-    .then((data) => {
-      res.write(data);
-      res.end();
-    })
-    .catch((error) => {
-      res.write(error.message);
-      res.end();
-    });
-    
-})
+// Route for the root URL
+app.get('/', (_, res) => {
+  res.send('Hello Holberton School!');
+});
 
-app.listen(1245, () => {
-  console.log('Server running');
+// Route for /students URL
+app.get('/students', (_, res) => {
+  const responseParts = ['This is the list of our students'];
+
+  countStudents(DB_FILE)
+    .then((report) => {
+      responseParts.push(report);
+      res.setHeader('Content-Type', 'text/plain');
+      res.send(responseParts.join('\n'));
+    })
+    .catch((err) => {
+      responseParts.push(err.message);
+      res.setHeader('Content-Type', 'text/plain');
+      res.send(responseParts.join('\n'));
+    });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
 });
 
 module.exports = app;
-
